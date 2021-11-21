7ecadaccd7e1802282ec024dbf239ed4481ba2b7
Rokas <rookas.rudzenskas@gmail.com>
2021-11-21 16:16:42 +0200

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Instagram</title>
+        <title>Instagram 🔥</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
