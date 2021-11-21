e8f2fa5761ab7b5900eff287e9b138b139a52c61
Rokas <rookas.rudzenskas@gmail.com>
2021-11-21 16:13:17 +0200

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Instagram</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
+          <a className="text-blue-600" href="https://byrookas.com">
             Rookas Instagram show
           </a>
         </h1>
