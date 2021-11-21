44c7344a661be48a48fb473e952531ce50cf696e
Rokas <rookas.rudzenskas@gmail.com>
2021-11-21 16:27:59 +0200

    Rokas

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Image from "next/image";
 
 const Header = () => {
         return (
                 <div>
                 {/*  Left  */}
+                        <div className="">
+                                <Image
+                                        src='https://links.papareact.com/ocw'
+                                        layout="fill"
+                                />
+                        </div>
                 {/*  Middle  */}
                 {/*  Right  */}
                 </div>
