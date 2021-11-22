be672a3e5af402c9dfbcc06789d7c7b7fffbe0e6
Rokas <rookas.rudzenskas@gmail.com>
2021-11-22 19:49:00 +0200

    Rokas

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -5,7 +5,7 @@ const MiniProfile = () => {
         <div className="flex items-center justify-between mt-14 ml-10">
             <img src="https://avatars.githubusercontent.com/u/38469892?v=4" alt="" className="rounded-full border p-[2px] h-16 w-16"/>
 
-            <div>
+            <div className="flex-1 mx-4">
                 <h2 className="font-bold">rookasrudzianskas</h2>
                 <h3 className="text-sm text-gray-400">Welcome to the Instagram</h3>
             </div>
