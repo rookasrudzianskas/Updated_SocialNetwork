f0600fc317cde845cdeaafec45a4819cf52c7f1d
Rokas <rookas.rudzenskas@gmail.com>
2021-11-22 12:14:38 +0200

    Rokas

--- components/Header.js
+++ components/Header.js
@@ -27,7 +27,7 @@ const Header = () => {
                       <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
                         <SearchIcon className='h-5 w-5 text-gray-500' />
                       </div>
-                      <input className="bg-gray-50 block w-full pl-10" type="text" placeholder="Search"/>
+                      <input className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black" type="text" placeholder="Search"/>
                   </div>
 
                   {/*        right      */}
