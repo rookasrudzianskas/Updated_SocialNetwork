8b8cf37ac713dd83620ec52d6f10bc923a0e49dc
Rokas <rookas.rudzenskas@gmail.com>
2021-11-22 12:12:36 +0200

    Rokas

--- components/Header.js
+++ components/Header.js
@@ -17,17 +17,17 @@ const Header = () => {
                       <Image src="https://links.papareact.com/ocw" layout={'fill'} objectFit={'contain'} />
                   </div>
 
-                  <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+                  <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
                       <Image src="https://links.papareact.com/jjm" layout={'fill'} objectFit={'contain'} />
                   </div>
 
                   {/*        middle     */}
 
-                  <div className="relative mt-1 p-3 rounded-md bg-red-500">
+                  <div className="relative mt-1 p-3 rounded-md">
                       <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
                         <SearchIcon className='h-5 w-5 text-gray-500' />
                       </div>
-                      <input type="text" placeholder="Search"/>
+                      <input className="bg-gray-50 block w-full pl-10" type="text" placeholder="Search"/>
                   </div>
 
                   {/*        right      */}
