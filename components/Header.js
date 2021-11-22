84d5d7098ddc689afb51de02cdf0990391c2fc9e
Rokas <rookas.rudzenskas@gmail.com>
2021-11-22 13:24:38 +0200

    Rokas

--- components/Header.js
+++ components/Header.js
@@ -36,10 +36,10 @@ const Header = () => {
                   {/*        right      */}
 
                   <div className="flex items-center justify-end space-x-4">
-                      <HomeIcon className='h-10 w-10' />
+                      <HomeIcon className='navBtn' />
                       <MenuIcon className='h-6 md:hidden cursor-pointer' />
 
-                      <PaperAirplaneIcon className='w-10 h-10' />
+                      <PaperAirplaneIcon className='navBtn' />
                   </div>
 
               </div>
