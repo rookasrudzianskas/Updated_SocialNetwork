889cf422e55e3d0eaca12b28aa5899a552d9ee32
Rokas <rookas.rudzenskas@gmail.com>
2021-11-22 09:00:47 +0200

    Rokas

--- components/Header.js
+++ components/Header.js
@@ -5,15 +5,20 @@ const Header = () => {
         return (
           <div className="">
               <div className="flex justify-between bg-white max-w-6xl">
-                  <div className="relative hidden lg:inline-grid w-24 h-24">
+                  <div className="relative hidden lg:inline-grid w-24 h-24 cursor-pointer">
                       <Image src="https://links.papareact.com/ocw" layout={'fill'} objectFit={'contain'} />
                   </div>
 
-                  <div className="relative w-10 h-10 lg:hidden flex-shrink-0">
+                  <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
                       <Image src="https://links.papareact.com/jjm" layout={'fill'} objectFit={'contain'} />
                   </div>
 
                   {/*        middle     */}
+
+                  <div>
+                      <input type="text" placeholder="Search"/>
+                  </div>
+
                   {/*        right      */}
               </div>
 
