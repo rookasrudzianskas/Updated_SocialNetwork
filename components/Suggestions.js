da6b0156f9b0b1bff9be871c8f67b7d6324ba6c1
Rokas <rookas.rudzenskas@gmail.com>
2021-11-22 19:51:58 +0200

    Rokas

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import faker from 'faker';
 
 const Suggestions = () => {
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() =>  {
+        // 🔥 on 🌛 🏃‍♂️
+        const suggestions = [...Array(5)].map((_, i) => (
+            {
+                ...faker.helpers.contextualCard(),
+                id: i,
+            }
+        ));
+
+        setSuggestions(suggestions);
+    }, []);
+
     return (
         <div>
 
