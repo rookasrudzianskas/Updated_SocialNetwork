a16d0290ff0165ba6fbe7d061fec833ad51b0d5e
Rokas <rookas.rudzenskas@gmail.com>
2021-11-22 14:51:41 +0200

    Rokas

--- components/Post.js
+++ components/Post.js
@@ -3,10 +3,10 @@ import {DotsHorizontalIcon} from "@heroicons/react/outline";
 
 const Post = ({username, caption, id, img, userImg}) => {
     return (
-        <div>
+        <div className="bg-white my-7 border rounded-sm">
             <div className="flex items-center p-5">
                 <img src={img} alt="" className="rounded-full h-12 w-12 object-contain border p-1 mr-3"/>
-                <p>{username}</p>
+                <p className="flex-1 font-bold">{username}</p>
                 <DotsHorizontalIcon className="h-5" />
             </div>
         </div>
