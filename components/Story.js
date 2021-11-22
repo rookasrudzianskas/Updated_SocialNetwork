71a37c305d80b070afbbe84eea1e5e626c07350f
Rokas <rookas.rudzenskas@gmail.com>
2021-11-22 14:12:24 +0200

    Rokas

--- components/Story.js
+++ components/Story.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Story = ({username, img}) => {
     return (
-        <div>
-            <img src={img} alt=""/>
+        <div className="">
+            <img className="h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2" src={img || 'https://avatars.githubusercontent.com/u/38469892?v=4'} alt=""/>
             <p>{username}</p>
         </div>
     );
