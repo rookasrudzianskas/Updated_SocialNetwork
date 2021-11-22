b8238506023d07b7c864e58a41d405157b6c28fc
Rokas <rookas.rudzenskas@gmail.com>
2021-11-22 13:51:20 +0200

    Rokas

--- components/Feed.js
+++ components/Feed.js
@@ -5,7 +5,7 @@ const Feed = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
         {/*    section  */}
-            <section>
+            <section className="col-span-2">
                 <Stories />
             </section>
 
