0e0c55c7861027403300097fff20bdcb619fb6a3
Rokas <rookas.rudzenskas@gmail.com>
2021-11-22 13:54:20 +0200

    Rokas

--- components/Stories.js
+++ components/Stories.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import faker from 'faker';
+
 
 const Stories = () => {
+
+    useEffect(() =>  {
+    //    this will 🔥 then component loads
+
+    }, []);
+
     return (
         <div>
 
