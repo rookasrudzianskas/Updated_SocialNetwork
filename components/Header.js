7261ff6ac0d5be368e7b2285bbe26eee5037a2bb
Rokas <rookas.rudzenskas@gmail.com>
2021-11-22 12:32:20 +0200

    Rokas

--- components/Header.js
+++ components/Header.js
@@ -8,11 +8,12 @@ import {
     PaperAirplaneIcon,
     MenuIcon,
 } from "@heroicons/react/outline";
+import {HomeIcon} from "@heroicons/react/solid";
 
 const Header = () => {
         return (
           <div className="">
-              <div className="flex justify-between bg-white max-w-6xl">
+              <div className="flex justify-between bg-white max-w-6xl lg:mx-auto">
                   <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
                       <Image src="https://links.papareact.com/ocw" layout={'fill'} objectFit={'contain'} />
                   </div>
@@ -23,14 +24,22 @@ const Header = () => {
 
                   {/*        middle     */}
 
-                  <div className="relative mt-1 p-3 rounded-md">
-                      <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-                        <SearchIcon className='h-5 w-5 text-gray-500' />
+                  <div className="max-w-xs">
+                      <div className="relative mt-1 p-3 rounded-md">
+                          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                              <SearchIcon className='h-5 w-5 text-gray-500' />
+                          </div>
+                          <input className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black rounded-md" type="text" placeholder="Search"/>
                       </div>
-                      <input className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black" type="text" placeholder="Search"/>
                   </div>
 
                   {/*        right      */}
+
+                  <div className="flex items-center">
+                      <HomeIcon className='h-10 w-10' />
+                      <HomeIcon className='h-10 w-10' />
+                  </div>
+
               </div>
 
           </div>
