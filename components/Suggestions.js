ee33e7592fa32853b186bd26095907b52871e83c
Rokas <rookas.rudzenskas@gmail.com>
2021-11-22 19:59:23 +0200

    Rokas

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -27,6 +27,14 @@ const Suggestions = () => {
                 suggestions.map(profile => (
                     <div key={profile.id} className="flex items-center justify-between mt-3">
                         <img src={profile.avatar} className="w-10 h-10 rounded-full border p-[2px]" alt=""/>
+
+                        <div className="flex-1 ml-4">
+                            <h2 className="font-semibold text-sm">{profile.username}</h2>
+                            <h3 className="text-sm text-gray-400">Works at {profile.company.name}</h3>
+                        </div>
+
+                        <button className="text-blue-400 font-semibold">Follow</button>
+
                     </div>
                 ))
             }
