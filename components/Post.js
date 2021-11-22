a98b1b3e03ef5722e20188470a434699093223ca
Rokas <rookas.rudzenskas@gmail.com>
2021-11-22 14:49:18 +0200

    Rokas

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {DotsHorizontalIcon} from "@heroicons/react/outline";
 
 const Post = ({username, caption, id, img, userImg}) => {
     return (
         <div>
-
+            <div>
+                <img src={img} alt="" className="rounded-full h-12 w-12 object-contain border p-1 mr-3"/>
+                <p>{username}</p>
+                <DotsHorizontalIcon className="h-5" />
+            </div>
         </div>
     );
 };
