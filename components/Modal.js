88c8e22b721d634673951470326539e949964da9
Rokas <rookas.rudzenskas@gmail.com>
2021-11-23 15:53:05 +0200

    Roaks

--- components/Modal.js
+++ components/Modal.js
@@ -44,6 +44,10 @@ const Modal = () => {
                         // ref={completeButtonRef}
                     >
 
+                        <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+
+                        </div>
+
                     </Transition.Child>
 
                 </div>
