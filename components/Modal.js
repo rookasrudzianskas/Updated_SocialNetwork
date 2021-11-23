4d2d8744a83e547f85fb298e9d02a2379f2d64eb
Rokas <rookas.rudzenskas@gmail.com>
2021-11-23 15:40:15 +0200

    Rokas

--- components/Modal.js
+++ components/Modal.js
@@ -26,9 +26,22 @@ const Modal = () => {
                         <Dialog.Overlay className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
                     </Transition.Child>
 
+                    {/* trick browser to center modals */}
                     <span className='hidden sm:inline-block sm:align-middle sm:h-screen'>
                         &#8203
                     </span>
+
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        enterTo="opacity-100 translate-y-0 sm:scale-100"
+                        leave='ease-in duration-200'
+                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    >
+
+                    </Transition.Child>
 
                 </div>
             </Dialog>
