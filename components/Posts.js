10edf2d0a3f96afab1c8a61133dd150150e19fcf
Rokas <rookas.rudzenskas@gmail.com>
2021-11-23 17:16:07 +0200

    Rokas

--- components/Posts.js
+++ components/Posts.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Post from "./Post";
 import {useState} from 'react';
+import {collection} from "@firebase/firestore";
+import {db} from "../firebase";
 
 const Posts = () => {
     const [posts, setPosts] = useState([]);
 
     useEffect(() =>  {
+        onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {
+           // realtime listening
+           setPosts(snapshot.docs);
+        });
 
-    }, []);
+        return unsubscribe();
+    }, [db]);
 
     return (
         <div>
