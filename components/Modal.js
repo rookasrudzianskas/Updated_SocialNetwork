9008e3278a94f87a8d52970707fdf83f6f9177b5
Rokas <rookas.rudzenskas@gmail.com>
2021-11-23 14:10:51 +0200

    Rokas

--- components/Modal.js
+++ components/Modal.js
@@ -7,8 +7,15 @@ const Modal = () => {
     const [open, setOpen] = useRecoilState(modalState);
 
     return (
-        <Transition.Root show={open} as={Fragment}>
+        <Transition.Root show={!!open} as={Fragment}>
+            <Dialog
+                as='div'
+                className='fixed z-10 inset-0 overflow-y-auto'
+                onClose={setOpen}
+            >
 
+                <h1>Hello</h1>
+            </Dialog>
         </Transition.Root>
     );
 };
