2aa6f15a9392860bcf2238f3ce090fad701ce6ac
Rokas <rookas.rudzenskas@gmail.com>
2021-11-23 18:59:12 +0200

    Rokas

--- components/Post.js
+++ components/Post.js
@@ -84,6 +84,9 @@ const Post = ({username, caption, id, img, userImg}) => {
             )}
 
             <p className="p-5 truncate">
+                {likes.length > 0 && (
+                    <p className="font-bold mb-1">{likes.length} likes</p>
+                )}
                 <span className="font-bold mr-1">{username} </span>{caption}
             </p>
 
