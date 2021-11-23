c3d29721a2a3968540313fe93dbb19e8f52c704e
Rokas <rookas.rudzenskas@gmail.com>
2021-11-23 16:07:28 +0200

    Rokas

--- components/Modal.js
+++ components/Modal.js
@@ -5,7 +5,8 @@ import { Dialog, Transition } from '@headlessui/react'
 
 const Modal = () => {
     const [open, setOpen] = useRecoilState(modalState);
-    let completeButtonRef = useRef(null)
+    let filePickerRef = useRef(null);
+    let captionRef = useRef(null);
 
 
     return (
@@ -46,8 +47,35 @@ const Modal = () => {
 
                         <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
                             <div>
+                                 <div>
+                                    <div className="mt-3 text-center sm:mt-5">
+                                        <Dialog.Title
+                                            as="h3"
+                                            className="text-lg leading-6 font-medium text-gray-900"
+                                        >
+                                            Upload a photo
+                                        </Dialog.Title>
 
+                                        <div>
+                                            <input
+                                                type="file"
+                                                ref={filePickerRef}
+                                                hidden
+                                                // onChange={addImageToPost}
+                                            />
+                                        </div>
 
+                                        <div className="mt-2">
+                                            <input
+                                                className="border-none focus:ring-0 w-full text-center"
+                                                type="text"
+                                                // ref={captionRef}
+                                                placeholder="Please enter the caption"
+                                            />
+                                        </div>
+
+                                    </div>
+                                 </div>
 
                                 <div className="mt-5 sm:mt-6">
                                     <button
@@ -55,7 +83,7 @@ const Modal = () => {
                                         disabled={false}
                                         className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none disabled:focus:ring-2 disabled:focus:ring-offset-2 disabled:focus:ring-gray-300 focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
                                     >
-                                        DONE
+                                        Upload the file
                                     </button>
                                 </div>
                             </div>
