d045f59854605d7d34bc5a688ddf453666572209
Rokas <rookas.rudzenskas@gmail.com>
2021-11-23 12:20:41 +0200

    ROkas

--- components/Modal.js
+++ components/Modal.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {modalState} from "../atoms/modalAtom";
+import {useRecoilState} from "recoil";
 
 const Modal = () => {
+    const [open, setOpen] = useRecoilState(modalState);
+
     return (
         <div>
-            
+
         </div>
     );
 };
