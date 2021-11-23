d236ccb7d45525c0f70a92c6f2a7d7676b239638
Rokas <rookas.rudzenskas@gmail.com>
2021-11-23 17:00:13 +0200

    Rokas

--- components/Modal.js
+++ components/Modal.js
@@ -154,10 +154,11 @@ const Modal = () => {
                                 <div className="mt-5 sm:mt-6">
                                     <button
                                         type="button"
-                                        disabled={false}
+                                        disabled={!selectedFile}
+                                        onClick={uploadPost}
                                         className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none disabled:focus:ring-2 disabled:focus:ring-offset-2 disabled:focus:ring-gray-300 focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
                                     >
-                                        Upload the file
+                                        {loading ? 'Uploading...' : 'Upload Post'}
                                     </button>
                                 </div>
                             </div>
