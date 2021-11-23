20f406be716a112052e052b7da07f6a4b7d0642f
Rokas <rookas.rudzenskas@gmail.com>
2021-11-23 12:22:28 +0200

    Rokas

--- components/Modal.js
+++ components/Modal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {modalState} from "../atoms/modalAtom";
 import {useRecoilState} from "recoil";
+import { Dialong, Transition } from "@headlessui/react";
 
 const Modal = () => {
     const [open, setOpen] = useRecoilState(modalState);
