51a5fc753f6cf6f39e55b7e0b8a01b63c82d422b
Rokas <rookas.rudzenskas@gmail.com>
2021-11-23 10:22:34 +0200

    Rokas

--- components/Feed.js
+++ components/Feed.js
@@ -3,10 +3,14 @@ import Stories from "./Stories";
 import Posts from "./Posts";
 import MiniProfile from "./MiniProfile";
 import Suggestions from "./Suggestions";
+import {useSession} from "next-auth/react";
 
 const Feed = () => {
+
+    const {data: session } = useSession();
+
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+        <main className={`grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto ${!session && "!grid-cols-1 !max-w-3xl"}`}>
         {/*    section  */}
             <section className="col-span-2">
                 <Stories />
@@ -15,11 +19,11 @@ const Feed = () => {
 
             <section className="hidden xl:inline-grid md:col-span-1">
                 <div className="fixed">
-                    <MiniProfile />
-                    <Suggestions />
+                    {session && <MiniProfile />}
+                    {session && <Suggestions />}
                 </div>
             </section>
-        </div>
+        </main>
     );
 };
 
