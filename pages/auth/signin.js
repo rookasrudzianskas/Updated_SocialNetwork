d35b46a3eef1113bd35adf5155e42c663640f505
Rokas <rookas.rudzenskas@gmail.com>
2021-11-23 09:25:46 +0200

    Rokas

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -5,13 +5,13 @@ import {getProviders, signIn as SignIntoProvider } from "next-auth/react";
 const signIn = ({ providers }) => {
     return (
         <>
-            {/*{Object.values(providers).map((provider) => (*/}
-            {/*    <div key={provider.name}>*/}
+            {Object.values(providers).map((provider) => (
+                <div key={provider.name}>
             {/*        <button onClick={() => SignIntoProvider(provider.id)}>*/}
             {/*            Sign in with {provider.name}*/}
             {/*        </button>*/}
-            {/*    </div>*/}
-            {/*))}*/}
+                </div>
+            ))}
         </>
     );
 };
