72033dae37f60af32d5bea6afcf089dc723d08c0
Rokas <rookas.rudzenskas@gmail.com>
2021-11-23 16:15:50 +0200

    Rokas

--- components/Modal.js
+++ components/Modal.js
@@ -53,7 +53,6 @@ const Modal = () => {
                                     onClick={() => filePickerRef.current.click()}
                                     className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer"
                                 >
-
                                     <CameraIcon
                                         className="h-6 w-6 text-red-600"
                                         aria-hidden="true"
