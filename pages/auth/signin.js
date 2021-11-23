7f615579d044a1a86dc44bcc864cfdf89a7d0790
Rokas <rookas.rudzenskas@gmail.com>
2021-11-23 10:08:56 +0200

    Rokas

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -7,13 +7,23 @@ const signIn = ({ providers }) => {
     return (
         <>
             <Header />
-            {Object.values(providers).map((provider) => (
-                <div key={provider.name}>
-                    <button className="p-3 bg-blue-500 rounded-lg text-white" onClick={() => SignIntoProvider(provider.id)}>
-                        Sign in with {provider.name}
-                    </button>
+
+            <div>
+
+                <img src="https://links.papareact.com/ocw" alt=""/>
+                
+                <div className="mt-40">
+                    {Object.values(providers).map((provider) => (
+                        <div key={provider.name}>
+                            <button className="p-3 bg-blue-500 rounded-lg text-white" onClick={() => SignIntoProvider(provider.id)}>
+                                Sign in with {provider.name}
+                            </button>
+                        </div>
+                    ))}
                 </div>
-            ))}
+            </div>
+
+
         </>
     );
 };
